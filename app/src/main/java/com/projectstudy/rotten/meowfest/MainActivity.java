392c47acd766a7f55894094addb8a2c05199808a
alc73 <alc73@students.uwf.edu>
2019-02-06 15:31:12 -0500

    initial commit, internet permission in manifest file and gradle build files set up

---
+++ app/src/main/java/com/projectstudy/rotten/meowfest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.projectstudy.rotten.meowfest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private String endpoint;
+    private String titleArr[];
+    private String descArr[];
+    private String dateArr[];
+    private String imgUrlArr[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        endpoint = "https://chex-triplebyte.herokuapp.com/api/cats?page=0";
+
+    }
+
+}
